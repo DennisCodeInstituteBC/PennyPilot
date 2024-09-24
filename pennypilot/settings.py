bdadbbeb00cbf46fab0ffef55edb94ad55eeb2ee
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-09-24 13:10:30 +0000

    adding static to setting

--- pennypilot/settings.py
+++ pennypilot/settings.py
@@ -19,6 +19,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["8000-denniscodein-pennypilot-rewm685nnj5.ws.codeinstitute-ide.net",
+ALLOWED_HOSTS = ["8000-denniscodein-pennypilot-5ri2dnzyl34.ws.codeinstitute-ide.net",
                 ".herokuapp.com"]
 
 # code to allow IDE and Heroku to pass CSRF Verification
@@ -132,7 +133,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 
 # Default primary key field type
