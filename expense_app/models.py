a5ab38f714b18f64e9df09a4d8021cd6c24edcf7
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-09-27 13:15:31 +0000

    adding database for expense_app > models.py for catagory, espense, notifications and linking users from allauth

--- expense_app/models.py
+++ expense_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth import get_user_model # importing users from allauth
+
+User = get_user_model()
 
 # Create your models here.
+
+
+
+# Category Model.py linked with Users from allauth
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Expense Model.py linked with Users(allauth) and Catergory
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
+    notes = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.amount}" #username and expense amount
+
+class Notifications(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reminder_type = models.CharField(max_length=100)
+    reminder_time = models.DateTimeField ()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notificaiton for {self.user.username} - {self.reminder_type}"
+
