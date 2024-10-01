6771df2997337efbef8a153954cb899c0ae41e7d
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-10-01 15:28:53 +0000

    adding css styling to dashboard page with updated css and hmlt file to fit the theme

--- expense_app/urls.py
+++ expense_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
     path('expense/', views.expense_view, name='expense'),
     path('account/', views.account_view, name='account')
 ]
