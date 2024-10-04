9db20f20e6be6830b4d0942d3569ac5bbf470920
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-10-04 12:13:48 +0000

    models.py update to meet CI Python Linter validation

--- expense_app/models.py
+++ expense_app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model # importing users from allauth
-from django.contrib.auth.models import User 
+# importing users from allauth
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 
 User = get_user_model()
 
-# Create your models here.
 
 # Category Model.py linked with Users from allauth
 class Category(models.Model):
@@ -12,10 +12,11 @@ class Category(models.Model):
     type = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
 
 # Expense Model.py linked with Users(allauth) and Catergory
 class Expense(models.Model):
@@ -33,7 +34,11 @@ class Expense(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    profile_image = models.ImageField(
+        upload_to='profile_images',
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.user.username
